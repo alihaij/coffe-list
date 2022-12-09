2363632f895e62ee790588d6d5af5973e57c0874
alihaij <a3haij@gmail.com>
2022-12-10 02:02:12 +0300

    project structure

---
+++ lib/model/coffee.dart
@@ -0,0 +1,13 @@
+class Coffee {
+  final int id;
+  final String title;
+  final String description;
+  final List<String> ingredients;
+  final String imgUrl;
+  Coffee(
+      {required this.id,
+      required this.title,
+      required this.description,
+      required this.ingredients,
+      required this.imgUrl});
+}
